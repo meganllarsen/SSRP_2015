2a5388634d35fb99836cd4bb2a8519b3148f54c8
Nathan Wisnoski <wisnoski@indiana.edu>
2015-07-16 09:58:34 -0400

    pushing raw and clean data from plate reader

--- bin/xlsx_to_csv.R
+++ bin/xlsx_to_csv.R
@@ -9,21 +9,22 @@ require(xlsx)
 
 convert <- function(filename){
   # Read in and parse .xlsx files from plate reader.
-  sheet <- read.xlsx(paste(getwd(),"data",filename, sep="/"), sheetIndex=1, header=FALSE)
+  sheet <- read.xlsx(paste(getwd(),"data","raw",filename, sep="/"), sheetIndex=1, header=FALSE)
   plate <- as.matrix(sheet[(19:26),(3:14)])
   rownames(plate) <- c("A", "B", "C", "D", "E", "F", "G", "H")
   colnames(plate) <- c("1", "2", "3", "4", "5", "6", 
                        "7", "8", "9", "10", "11", "12")
   
   # Save a .csv of the plate output for reading in later.
-  out.filename <- paste(getwd(),"data", sub(".xlsx", replacement=".txt", filename), sep='/')
-  write.table(plate, file=out.filename)
+  out.filename <- paste(getwd(),"data","cleaned", sub(".xlsx", replacement=".txt", filename), sep='/')
+  write.table(plate, file=out.filename, col.names=FALSE, row.names=FALSE)
 }
 
 # Generates a list of .xlsx spreadsheets from plate reader
-file.list <- list.files("./data/", pattern="*.xlsx")
+file.list <- list.files("./data/raw", pattern="*.xlsx")
 
 # Loop through and convert spreadsheets into csv files
 for (in.file in file.list){
   try(convert(in.file), silent = TRUE)
 }
+
